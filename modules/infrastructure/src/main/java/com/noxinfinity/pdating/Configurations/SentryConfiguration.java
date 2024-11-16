c64e17df520e85a539e00c62eddccd202646cebb
Luong Tuan Anh <dulieu.vblc@gmail.com>
2024-11-16 18:35:23 +0700

    add: codegen

---
+++ modules/infrastructure/src/main/java/com/noxinfinity/pdating/Configurations/SentryConfiguration.java
@@ -0,0 +1,9 @@
+package com.noxinfinity.pdating.Configurations;
+
+import io.sentry.spring.jakarta.EnableSentry;
+import org.springframework.context.annotation.Configuration;
+
+@EnableSentry(dsn = "https://df3f21d2a02102dcb4281a977fff7695@o4508307637731328.ingest.de.sentry.io/4508307641532496")
+@Configuration
+class SentryConfiguration {
+}
