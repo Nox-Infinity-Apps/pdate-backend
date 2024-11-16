245a4344b9e13ca62438c982b6f9d472e1b3b4c1
Luong Tuan Anh <dulieu.vblc@gmail.com>
2024-11-16 15:22:36 +0700

    add: codegen

---
+++ modules/infrastructure/src/main/java/com/noxinfinity/pdating/ThirdServices/FirebaseInitialization.java
@@ -0,0 +1,30 @@
+package com.noxinfinity.pdating.ThirdServices;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+
+@Configuration
+public class FirebaseInitialization {
+
+    @Bean
+    public FirebaseApp initialization() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("/Users/n0x/IDEAIJ/GCGV/modules/app/src/main/resources/serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            return FirebaseApp.initializeApp(options);
+        } catch (Exception error) {
+            error.printStackTrace();
+            throw new RuntimeException("Failed to initialize Firebase", error);
+        }
+    }
+}
