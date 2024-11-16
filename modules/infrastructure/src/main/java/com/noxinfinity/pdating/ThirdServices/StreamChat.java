3d4f5a081d92d2323aa89ba7fbaff2462f006f3c
Luong Tuan Anh <dulieu.vblc@gmail.com>
2024-11-16 14:05:47 +0700

    add: v2 base

---
+++ modules/infrastructure/src/main/java/com/noxinfinity/pdating/ThirdServices/StreamChat.java
@@ -0,0 +1,9 @@
+package com.noxinfinity.pdating.ThirdServices;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StreamChat {
+
+}
